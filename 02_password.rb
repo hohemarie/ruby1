b88859e3cc69f19423e295e235988d9b1c33fcbb
hohemarie <marie.goudon@laposte.net>
2019-01-10 22:36:52 +0100

    ajout de l'exo 2

--- 02_password.rb
+++ 02_password.rb
@@ -1,13 +1,26 @@
 motdepasse="megagigapassword"
 #signup
+def signup
 print "entrez le password sil vous plait : "
 mdp=gets.chomp
+end
 
+def login
 #login
 while mdp != motdepasse do
 	print "vous vous etes trompes. entrez encore le password : "
 	mdp=gets.chomp
 end
+end
 
+def welcome_screen
 #welcome screen
 puts "felicitations vous avez entre le bon mot de passe, voici quelques infos de la NSA"
+end
+
+def perform
+signup
+login
+welcome_screen
+end
+perform
